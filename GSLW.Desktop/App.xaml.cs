2a7e83d9baf77a3998d6b319c37ac536311b2cd2
Rômulo Mauriz <romulo.mauriz@gmail.com>
2025-05-30 11:56:04 -0300

    GSLW-1: criar solução principal GSLW.sln e adicionar estrutura básica de projetos
    GSLW-1: adicionar projeto GSLW.Api (ASP.NET Core WebAPI)
    
    GSLW-1: adicionar projeto GSLW.Desktop (.NET MAUI)
    
    GSLW-1: adicionar projeto GSLW.Core (biblioteca de classes para lógica central)
    
    GSLW-1: estruturar GSLW.Core com pastas Models, Services e Helpers
    
    GSLW-1: adicionar referências do Core à Api e Desktop

---
+++ GSLW.Desktop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GSLW.Desktop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GSLW.Desktop/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GSLW.Desktop
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
